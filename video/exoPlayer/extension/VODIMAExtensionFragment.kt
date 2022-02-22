012ad8f7ad4a882674cfd0ed4915976ee92ed12e
Martin-Bruntsch <49907710+Martin-Bruntsch@users.noreply.github.com>
2022-02-22 11:12:20 +0100

    new Android demo app version has been released

---
+++ video/exoPlayer/extension/VODIMAExtensionFragment.kt
@@ -0,0 +1,54 @@
+package com.gfk.s2s.demo.video.exoPlayer.extension
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gfk.s2s.demo.MainActivity
+import com.gfk.s2s.demo.s2s.R
+import com.gfk.s2s.demo.video.exoPlayer.BaseVideoFragment
+import com.gfk.s2s.exoplayer.ExoplayerExtension
+import com.gfk.s2s.s2sagent.S2SConfig
+
+class VODIMAExtensionFragment : BaseVideoFragment() {
+
+    override val videoURL = "https://demo-config-preproduction.sensic.net/video/video3.mp4"
+    private val configUrl = "https://demo-config-preproduction.sensic.net/s2s-android.json"
+    private val mediaId = "s2s-exoplayer-android-demo"
+    private val contentId = "default"
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        (activity as? MainActivity)?.supportActionBar?.title =
+            getString(R.string.fragment_title_vod)
+        return inflater.inflate(R.layout.video_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adURL = getString(R.string.ad_vmap_pods)
+        super.prepareVideoPlayer()
+
+        val config = S2SConfig(
+            mediaId,
+            configUrl,
+            true,
+            null
+        )
+
+        val extension = ExoplayerExtension(
+            exoPlayer!!,
+            config,
+            contentId,
+            hashMapOf(),
+            requireContext(),
+            this,
+        )
+
+        extension.activateNativeAdSupport()
+    }
+}
